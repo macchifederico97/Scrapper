b9141a881ab9766ecb7560d43bac8151031b7d30
Federico Macchi <macchifederico@libero.it>
2025-10-21 17:42:22 +1100

    fix

--- app/legacy/WebService/WSPipelineFile.py
+++ app/legacy/WebService/WSPipelineFile.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from legacy.WebService.WSPipelineGetID import getID_pipelines
 from playwright.sync_api import sync_playwright
 from pathlib import Path
 import json
@@ -30,11 +31,12 @@ def getPipelineNames(page, bifrost_instance:str, statusFilter: bool):
     page.goto(f"https://app.eu.visualfabriq.com/bifrost/{bifrost_instance}/pipelines")
 
     page.wait_for_load_state()
-    page.wait_for_timeout(8000)
 
     if statusFilter == True:
         filter_status_enabled(page)
-
+    else:
+        page.wait_for_timeout(5000)
+        
     exit_loop = False
     while not exit_loop:
         elements = page.locator(".bifrostcss-eXwpzm.undefined")
@@ -72,66 +74,24 @@ def getPipelineID(pipelineName: str, page):  #Lightweight function of scrape_pip
     page.locator(".bifrostcss-fFaJCf").nth(6).click()  #Opening the pipeline history            
 
     page.wait_for_load_state()
-    page.wait_for_timeout(2000)     #TEST
+    #page.wait_for_timeout(2000)     #TEST
 
     pipelineID = page.url.split("/")[-2]
 
     page.locator(".bifrostcss-fIrYkC").click()  #Pipeline-history page close
-    page.wait_for_timeout(3000)
+    page.wait_for_timeout(2000)
 
 
     return pipelineName, pipelineID
 
 
-def setFileMappingPy(bifrost_instance: str, headlessPar: bool):
+def setFileMappingPy(bifrost_instance: str, filterEnabled: bool, headlessPar: bool):
     """
     Scrapes the last pipeline execution filtered by name.
     Returns a dictionary with start time, finish time, and duration in minutes.
     """
-
-    # OPENING BROWSER
-    with sync_playwright() as p:
-        browser = p.chromium.launch(
-            headless=headlessPar
-            , args=["--no-sandbox", "--ignore-certificate-errors"])
-        context = browser.new_context(storage_state="state.json", device_scale_factor=1)
-        page = context.new_page()
-        page.set_viewport_size({"width": 1600, "height": 1200})
-
-        pipelineNames = getPipelineNames(page, bifrost_instance, True)  #Getting the names of all the pipelines in Bifrost
-
-        outputList = [] #Output list containing pipeline information dict
-
-        # Go to the page
-        page.goto(f"https://app.eu.visualfabriq.com/bifrost/{bifrost_instance}/pipelines")
-
-        for pipelineName in pipelineNames:
-                pipelineName, pipelineID = getPipelineID(pipelineName, page)
-                pipeDict = {"pipeline_name": pipelineName, "pipeline_id": pipelineID}
-                outputList.append(pipeDict)     #Creation of the dict with pipeline information and appended to the output list
-        browser.close()
-
-        # Save output to JSON file
-
-        # Percorso completo
-        cartella = Path(f"client/{bifrost_instance}")
-        cartella.mkdir(parents=True, exist_ok=True)
-        file_path = cartella / "pipeline.json"
-        
-        # Timestamp ISO 8601
-        timestamp = datetime.now().isoformat(timespec="seconds")
-
-        # Struttura finale
-        fileData = {
-            "last_updated": timestamp,
-            "pipelines": outputList
-        }
-
-        # Scrittura
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(fileData, f, indent=4)
-
-        return outputList
+    print("Starting setFileMappingPy function")
+    return getID_pipelines(bifrost_instance, filterEnabled,  headlessPar)
 
 
 #print(setFileMappingPy("nttdata", False))    #DEBUG & TESTING
